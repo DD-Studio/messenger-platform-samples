f9f350eae0e309f8ce16dd0dbb36607a56163c5f
DerekNien <aegis60918@gmail.com>
2016-06-16 23:42:54 +0800

    - Fix: bug

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const
   crypto = require('crypto'),
   express = require('express'),
   https = require('https'),
-  request = require('request');
+  request = require('request'),
   watson = require('watson-developer-cloud');
 
 var app = express();
